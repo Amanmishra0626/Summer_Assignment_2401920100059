1cd07c32c63d69d7bb86f012d99ed8104f5ae488
Amanmishra0626 <mishra0626aman@gmail.com>
2026-06-01 13:03:47 +0530

    Update Remove_Duplicates_from_Sorted_Array.cpp

--- Week_1/Day_1/Remove_Duplicates_from_Sorted_Array.cpp
+++ Week_1/Day_1/Remove_Duplicates_from_Sorted_Array.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int newInd=1,copy=1;
+        if(nums.empty()){
+            return 0;
+        }
         for(copy=1;copy<nums.size();copy++){
             if(nums[copy]!=nums[copy-1]){
                 nums[newInd]=nums[copy];
