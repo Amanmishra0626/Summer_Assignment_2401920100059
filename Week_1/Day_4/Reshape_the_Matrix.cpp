97c5960253fd6ff2fa94f0bbccd5c0931c4c1e85
Amanmishra0626 <mishra0626aman@gmail.com>
2026-06-04 12:45:39 +0530

    Create Reshape_the_Matrix.cpp

---
+++ Week_1/Day_4/Reshape_the_Matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        if(r*c!=mat.size()*mat[0].size()){
+            return mat;
+        }
+        vector<vector<int>> newMat(r,vector<int>(c,0));
+        int x=0,y=0;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(y<c){
+                    newMat[x][y++] =mat[i][j];
+                }
+                else {
+                    x++;
+                    y=0;
+                    newMat[x][y++]=mat[i][j];
+                }
+            }
+        }
+        return newMat;
+    }
+};
